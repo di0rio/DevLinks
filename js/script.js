07f8c62f5eb866f53bad64d0eab39b4fe1a37c1c
di0rio <caua.diorio29@gmail.com>
2023-08-10 09:43:15 -0300

    ajustes da página mobile-troca img-acessibilidade

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 var toggleMod = () => {
-    const html = document.documentElement;
-    html.classList.toggle("light");
-}
+  const html = document.documentElement;
+  html.classList.toggle("light");
+
+  const isLight = html.classList.contains("light");
+  const srcValue = isLight ? "../img/imagemFundoBranco.jpg": "../img/motinha.jpg";
+  const altValue = isLight ? "Imagem de perfil no modo claro com um rosto perto, com ohos grandes (foto tirada de cima)": "Imagem com uma pessoa na moto de uma criança";
+
+  document.querySelector("#profile img").setAttribute("src", srcValue);
+  document.querySelector("#profile img").setAttribute("alt", altValue);
+};
